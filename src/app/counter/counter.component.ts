693019110626365c027eb6b2ba7a6a1d91978dd6
David Chambers <dlchambersjr@gmail.com>
2018-12-13 16:43:13 -0800

    counter add and works

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+  counter = 0;
+
+  onSelect(value): void {
+    this.counter = this.counter + value;
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
